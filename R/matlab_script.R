a16d8a8f7d86d52135d99875eaceac0cb579fa9c
muschellij2 <muschellij2@gmail.com>
2015-04-29 16:58:07 -0400

    Fixing rvec_to_matlab

--- R/matlab_script.R
+++ R/matlab_script.R
@@ -121,9 +121,9 @@ rvec_to_matlabclist = function(x, matname = NULL){
 rvec_to_matlab = function(x, row = FALSE,
                           matname = NULL){
   x = paste0(x, ifelse(row, ",", ";"))
+  x = paste(x, collapse= " ")
   x = str_trim(x)
   x = gsub(paste0(ifelse(row, ",", ";"), "$"), "", x)
-  x = paste(x, collapse= " ")
   x = paste0("[", x, "];")
   if (!is.null(matname)) x = paste0(matname, " = ", x)
   x
