2263fc2f3d351cf6f6bf2532db3bab807987ebc5
muschellij2 <muschellij2@gmail.com>
2017-08-31 10:15:42 -0400

    Bumpgin version.

--- R/matlab_script.R
+++ R/matlab_script.R
@@ -152,16 +152,22 @@ rvec_to_matlabclist = function(x, matname = NULL){
 #' status
 #' @param x Numeric vector of values
 #' @param row Create row vector instead of column vector
+#' @param sep separator to use to separate cells.  Will override row
+#' argument
 #' @param matname Object in matlab to be assigned
 #' @export
 #' @return Character scalar of matlab code
 #' @import stringr
 rvec_to_matlab = function(x, row = FALSE,
+                          sep = NULL,
                           matname = NULL){
-  x = paste0(x, ifelse(row, ",", ";"))
-  x = paste(x, collapse= " ")
+  if (!is.null(sep)) {
+    sep = ifelse(row, ",", ";")
+  }
+  x = paste0(x, sep)
+  x = paste(x, collapse = " ")
   x = str_trim(x)
-  x = gsub(paste0(ifelse(row, ",", ";"), "$"), "", x)
+  x = gsub(paste0(sep, "$"), "", x)
   x = paste0("[", x, "];")
   if (!is.null(matname)) x = paste0(matname, " = ", x)
   x
